718577f31bd187cbb6222989b70049a9c0019998
harshil parmar <harshilparmar80@gmail.com>
2022-07-20 02:03:20 -0400

    cart,category,item model

---
+++ src/main/java/com/grocery/model/Item.java
@@ -0,0 +1,35 @@
+package com.grocery.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "item_id")
+	private int itemId;
+	@Column(nullable = false,unique = true)
+	private String name;
+	@Column(nullable = false)
+	private double price;
+	private String photo;
+	
+	private float discount;
+	private Date offerValidTill;
+	
+	@Column(nullable = false)
+	private int quantity = 0;
+	
+	private String description;
+		
+}
